714f9694d739f64b500ae7fbd5bb96f261c0aaac
unknown <rmtestard@gmail.com>
2019-05-25 17:12:41 +0200

    sharing styles using _app

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import BaseLayout from '../components/layout/BaseLayout';
 
-import { Container, Button } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'reactstrap';
 
 class Index extends Component {
   render() {
     return (
       <BaseLayout>
-        <Container>
-          <Button color="danger">Danger !</Button>
-        </Container>
+        <Container />
       </BaseLayout>
     );
   }
