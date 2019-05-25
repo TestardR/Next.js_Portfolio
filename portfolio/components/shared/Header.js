e8d25c1003e2c8ee149f62624983a1c5ac257fbf
unknown <rmtestard@gmail.com>
2019-05-25 17:50:53 +0200

    Header integration

--- portfolio/components/shared/Header.js
+++ portfolio/components/shared/Header.js
@@ -1,36 +1,3 @@
-// import React, { Component, Fragment } from 'react';
-
-// class Header extends Component {
-//   render() {
-//     return (
-//       <Fragment>
-
-//         <Link href="/about">
-//           <a className="routes">About</a>
-//         </Link>
-//         <Link href="/portfolios">
-//           <a className="routes">Portfolios</a>
-//         </Link>
-//         <Link href="/blogs">
-//           <a className="routes">Blogs</a>
-//         </Link>
-//         <Link href="/cv">
-//           <a className="routes">CV</a>
-//         </Link>
-//         <style jsx>
-//           {`
-//             a {
-//               font-size: 20px;
-//             }
-//           `}
-//         </style>
-//       </Fragment>
-//     );
-//   }
-// }
-
-// export default Header;
-
 import React from 'react';
 import Link from 'next/link';
 import {
