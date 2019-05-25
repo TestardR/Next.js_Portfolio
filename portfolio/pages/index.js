cb995e633beeb847a1951138d8aa7b4bffb6de88
unknown <rmtestard@gmail.com>
2019-05-25 17:39:48 +0200

    styling with typed.js

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import BaseLayout from '../components/layout/BaseLayout';
-
+import Typed from 'react-typed';
 import { Container, Row, Col } from 'reactstrap';
 
+import BaseLayout from '../components/layout/BaseLayout';
+
 class Index extends Component {
+  constructor(props) {
+    super(props);
+
+    this.roles = [
+      'Full-Stack Developer',
+      'Software Engineer',
+      'Tech Lover',
+      'Team Leader'
+    ];
+  }
+
   render() {
     return (
       <BaseLayout className="cover">
@@ -38,11 +50,25 @@ class Index extends Component {
               <Col md="6" className="hero-welcome-wrapper">
                 <div className="hero-welcome-text">
                   <h1>
-                    Welcome to the portfolio website of Romain Testard.
+                    Welcome to the website of Romain Testard.
+                    <br />
                     <br />
                     Get informed, collaborate and discover projects I was
                     working on through the years!
                   </h1>
+                  <br />
+                  <Typed
+                    className="self-typed"
+                    loop
+                    typeSpeed={60}
+                    backSpeed={60}
+                    strings={this.roles}
+                    backDelay={800}
+                    fadeOutDelay={100}
+                    loopCount={0}
+                    showCursor
+                    cursorChar="|"
+                  />
                 </div>
                 <div className="hero-welcome-bio">
                   <h1>Let's take a look at my work.</h1>
