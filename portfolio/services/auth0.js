79bb85176bf073850194a76b734ff5cd4c287477
unknown <rmtestard@gmail.com>
2019-05-26 15:21:18 +0200

    save tokens in cookies

--- portfolio/services/auth0.js
+++ portfolio/services/auth0.js
@@ -1,4 +1,5 @@
 import auth0 from 'auth0-js';
+import Cookies from 'js-cookie';
 
 class Auth0 {
   constructor() {
@@ -11,7 +12,9 @@ class Auth0 {
     });
 
     this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
     this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
   }
 
   handleAuthentication() {
@@ -28,12 +31,34 @@ class Auth0 {
     });
   }
 
-  setSession() {
-    // Save tokens
+  setSession(authResult) {
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+
+    Cookies.set('user', authResult.idTokenPayload);
+    Cookies.set('jwt', authResult.idToken);
+    Cookies.set('expiresAt', expiresAt);
+  }
+
+  logout() {
+    Cookies.remove('user');
+    Cookies.remove('jwt');
+    Cookies.remove('expiresAt');
+
+    this.auth0.logout({
+      returnTo: '',
+      clientID: 'K1FnFg6gWHUFC8Nr5Ba2KLYGnuYrA1bm'
+    });
   }
 
   login() {
     this.auth0.authorize();
+  }
+
+  isAuthenticated() {
+    const expiresAt = Cookies.getJSON('expiresAt');
+    return new Date().getTime() < expiresAt;
   }
 }
 
