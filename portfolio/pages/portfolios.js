2d5f5cf09679cdff73b1753c926c44882c33b967
unknown <rmtestard@gmail.com>
2019-05-29 12:45:26 +0200

    Portfolio Card Styling

--- portfolio/pages/portfolios.js
+++ portfolio/pages/portfolios.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from '../routes';
+import {
+  Col,
+  Row,
+  Card,
+  CardHeader,
+  CardBody,
+  CardText,
+  CardTitle
+} from 'reactstrap';
 
 import BaseLayout from '../components/layout/BaseLayout';
 import BasePage from '../components/layout/BasePage';
@@ -22,13 +31,29 @@ class Portfolios extends Component {
   }
 
   renderPosts(posts) {
-    return posts.map(post => {
+    return posts.map((post, index) => {
       return (
-        <li key={post.id}>
-          <Link route={`/portfolio/${post.id}`}>
-            <a style={{ fontSize: '20px' }}>{post.title}</a>
-          </Link>
-        </li>
+        <Col md="4">
+          <React.Fragment key={index}>
+            <span>
+              <Card className="portfolio-card">
+                <CardHeader className="portfolio-card-header">
+                  Some Position {index}
+                </CardHeader>
+                <CardBody>
+                  <p className="portfolio-card-city"> Some Location {index} </p>
+                  <CardTitle className="portfolio-card-title">
+                    Some Company {index}
+                  </CardTitle>
+                  <CardText className="portfolio-card-text">
+                    Some Description {index}
+                  </CardText>
+                  <div className="readMore"> </div>
+                </CardBody>
+              </Card>
+            </span>
+          </React.Fragment>
+        </Col>
       );
     });
   }
@@ -37,8 +62,8 @@ class Portfolios extends Component {
     const { posts } = this.props;
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage title="Portfolios">
-          <ul>{this.renderPosts(posts)}</ul>
+        <BasePage title="Portfolios" className="portfolio-page">
+          <Row>{this.renderPosts(posts)}</Row>
         </BasePage>
       </BaseLayout>
     );
